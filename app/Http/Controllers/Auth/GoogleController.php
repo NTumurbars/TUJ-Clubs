8e6df50e5f55b2851e0cd00261bf5d2e68968ed3
Thelilrex <137387550+Thelilrex@users.noreply.github.com>
2024-11-08 10:27:27 +0900

    added some comments to Users and googleController

--- app/Http/Controllers/Auth/GoogleController.php
+++ app/Http/Controllers/Auth/GoogleController.php
@@ -19,17 +19,18 @@ class GoogleController extends Controller
     {
         try {
             $user = Socialite::driver('google')->user();
-            $finduser = User::where('google_id', $user->id)->first();
-
+            $finduser = User::where('google_id', $user->id)->first(); // This is sql query for searching ppl inside the db
+            //If found we need to let them access the stuff
             if ($finduser) {
-                Auth::login($finduser);
-                return redirect()->intended('dashboard');
-            } else {
+                Auth::login($finduser);//May be they are tryna authenticate the user
+                return redirect()->intended('dashboard'); // This is to redirect the user to a page in this case dashboard
+            } //Else we ask them to use the google service again 
+            else {
                 $newUser = User::create([
                     'name' => $user->name,
                     'email' => $user->email,
-                    'google_id'=> $user->id,
-                    'password' => encrypt('123456dummy')
+                    'google_id'=> $user->id, // might not need it 
+                    'password' => encrypt('123456dummy') //We odnt need it
                 ]);
 
                 Auth::login($newUser);
