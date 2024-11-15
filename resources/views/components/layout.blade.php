aed92b93e636c4603b051ed26e197aed660fe009
NTumurbars <tumurbarsnarantuya@gmail.com>
2024-11-15 15:04:11 +0900

    profile functionality

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -55,10 +55,9 @@
                             <div class="hidden absolute right-0 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
                                 role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button"
                                 tabindex="-1">
-                                <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                                    role="menuitem">Your Profile</a>
-                                <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                                    role="menuitem">Settings</a>
+                                <a href="{{ route('profile.show') }}"
+                                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Your
+                                    Profile</a>
                                 <form method="post" action="{{ route('logout') }}">
                                     @csrf
                                     <button type="submit"
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Auth\GoogleController;
+use App\Http\Controllers\ProfileController;
+
+use Illuminate\Http\Request;
+
+use Illuminate\Http\RedirectResponse;
+
+use Illuminate\Support\Facades\Auth;
+
 
 // Route::get('/', function () {
 //     return view('posts.index');
@@ -14,10 +21,27 @@ use App\Http\Controllers\Auth\GoogleController;
 
 Route::view('/', 'posts.index')->name('home');
 Route::view('login', 'auth.login')->name('login');
+
+
 Route::get('login/google', [GoogleController::class, 'redirectToGoogle'])->name('login.google');
 Route::get('login/google/callback', [GoogleController::class, 'handleGoogleCallback']);
 Route::post('/logout', function (Request $request) {
+
     Auth::logout();
+
+
+    $request->session()->invalidate();
+
+
+    $request->session()->regenerateToken();
+
 
     return redirect('/login');
 })->name('logout');
+
+
+Route::get('/profile', [ProfileController::class, 'show'])->name('profile.show');
+    
+Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
+    
+Route::post('/profile/edit', [ProfileController::class, 'update'])->name('profile.update');
